c5032107a4504dba04c2d23fd0221a36da38e6cf
jdw09 <razion0304@naver.com>
2025-10-28 22:29:03 +0900

    딕셔너리 파일 생성

---
+++ src/pages/Dictionary/Dictionary.style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+`
+
+export const DictionaryContainer = styled.div`
+    display: flex;
+    height: 100%;
+`
+
+export const QuizContainer = styled.div`
+    display: flex;
+    height: 100%;
+`
--- src/pages/Dictionary/Dictionary.tsx
+++ src/pages/Dictionary/Dictionary.tsx
@@ -1,7 +1,15 @@
+import * as S from "./Dictionary.style"
+
 function Dictionary() {
     return (
-        <>
-        </>
+        <S.Container>
+            <S.DictionaryContainer>
+                딕셔너리
+            </S.DictionaryContainer>
+            <S.QuizContainer>
+                퀴즈
+            </S.QuizContainer>
+        </S.Container>
     )
 }
 
