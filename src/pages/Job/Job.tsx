be3f0d91e24e3ffd81368a83389456be8710637f
seunga418 <seungab77@gmail.com>
2025-12-01 21:45:15 +0900

    Update Job.tsx

--- src/pages/Job/Job.tsx
+++ src/pages/Job/Job.tsx
@@ -57,8 +57,7 @@ export default function Job() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaWQiOjgsImlhdCI6MTc2NDUwMTg5MX0.lhr8DsqdXfU13bJaEV595bpd9s2x02pdXg9ZIsDoQsU",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaWQiOjgsImlhdCI6MTc2NDUwMTg5MX0.lhr8DsqdXfU13bJaEV595bpd9s2x02pdXg9ZIsDoQsU",
         },
         body: JSON.stringify({
           email,
