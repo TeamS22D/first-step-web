f53af895492a9ef4c2eb94c263220c7c62f4aa8c
jdw09 <razion0304@naver.com>
2025-12-06 14:36:55 +0900

    refactor: Cookie get / remove 시 라이브러리 사용

--- src/hooks/cookies.ts
+++ src/hooks/cookies.ts
@@ -1,16 +1,11 @@
 import Cookie from "js-cookie"
 
 export const getCookie = (name: string): string | null => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    return parts.pop()?.split(";").shift() || null;
-  }
-  return null;
+  return Cookie.get(name) ?? null
 };
 
 export const deleteCookie = (name: string) => {
-  document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+  Cookie.remove(name);
 };
 
 export const setAccessToken = (token: string) => {
