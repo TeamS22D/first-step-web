73bdefe980d46a653d4b5a8f5d269f58dfbaa3ac
jdw09 <razion0304@naver.com>
2025-12-06 14:38:31 +0900

    feat: 인증 관련 Cookie 보안설정

--- src/hooks/cookies.ts
+++ src/hooks/cookies.ts
@@ -10,17 +10,17 @@ export const deleteCookie = (name: string) => {
 
 export const setAccessToken = (token: string) => {
   // Cookie.set("accessToken", token, {expires: 15 / (24 * 60)}) // 15분 뒤 만료
-  Cookie.set("accessToken", token, {expires: 15 / (24 * 60)}) // 15분 뒤 만료
+  Cookie.set("accessToken", token, {expires: 15 / (24 * 60), secure: true, sameSite: 'strict'}) // 15분 뒤 만료
 }
 
 export const setRefreshToken = (token: string) => {
-  Cookie.set("refreshToken", token, {expires: 7}) // 7일 뒤 만료
+  Cookie.set("refreshToken", token, {expires: 7, secure: true, sameSite: 'strict'}}) // 7일 뒤 만료
 }
 
 export const setEmail = (email: string) => {
-  Cookie.set("email", email, {expires: 7}) // 7일 뒤 만료
+  Cookie.set("email", email, {expires: 7, secure: true, sameSite: 'strict'}}) // 7일 뒤 만료
 }
 
 export const setUserId = (id: string) => {
-  Cookie.set("userId", id, {expires: 7}) // 7일 뒤 만료
+  Cookie.set("userId", id, {expires: 7, secure: true, sameSite: 'strict'}}) // 7일 뒤 만료
 }
