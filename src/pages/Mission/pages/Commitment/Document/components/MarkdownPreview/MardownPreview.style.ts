a955deb35fe31401a9810eb7fe332e5237f1993e
민선영 <ttt4306@gmail.com>
2025-12-13 02:13:38 +0900

    h1, h2.. 재정의

--- src/pages/Mission/pages/Commitment/Document/components/MarkdownPreview/MardownPreview.style.ts
+++ src/pages/Mission/pages/Commitment/Document/components/MarkdownPreview/MardownPreview.style.ts
@@ -11,4 +11,8 @@ export const Preview = styled.div`
   letter-spacing: -0.004em;
   word-break: keep-all;
   overflow-wrap: break-word;
+
+  h1, h2, h3, h4, h5, h6 {
+    all: revert;      
+  }
 `;
