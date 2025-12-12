376eed940790c0671c1ac02d0090af543e98efa9
민선영 <ttt4306@gmail.com>
2025-12-13 02:31:29 +0900

    bold 및 들여쓰기 재정의

--- src/pages/Mission/pages/Commitment/Document/components/MarkdownPreview/MardownPreview.style.ts
+++ src/pages/Mission/pages/Commitment/Document/components/MarkdownPreview/MardownPreview.style.ts
@@ -15,4 +15,19 @@ export const Preview = styled.div`
   h1, h2, h3, h4, h5, h6 {
     all: revert;      
   }
+
+  ul, ol {
+    list-style: revert;      /* 불릿, 번호 복구 */
+    margin-left: 1.2rem;     /* 목록 들여쓰기 복구 */
+    padding-left: 1rem;
+  }
+
+  li {
+    margin: 0.25rem 0;
+  }
+
+  strong, b {
+    font-weight: revert;   /* 굵기 복구 */
+  }
+
 `;
