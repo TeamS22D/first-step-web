4244cbf1c6e89f059a6be14e87bc2185fa913a36
jdw09 <razion0304@naver.com>
2025-12-14 01:08:36 +0900

    remove: router.tsx의 불필요한 import 제거

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,14 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import App from "./App";
-
 import Login from "./pages/Auth/pages/Login/Login";
 import Register from "./pages/Auth/pages/Register/Register";
 import AuthLayout from "./components/AuthLayout/AuthLayout";
-
 import NotFound from "./pages/NotFound/NotFound";
-
 import Home from "./pages/Home/Home";
 import Dictionary from "./pages/Dictionary/Dictionary";
 import Missions from "./pages/Missions/Missions";
-import MissionList from "./components/Missions/components/MissionList";
 import Feedback from "./pages/Feedback/Feedback";
-
 import Occupation from "./pages/Occupation/Occupation";
 import Job from "./pages/Job/Job";
 import Profile from "./pages/Profile/Profile";
